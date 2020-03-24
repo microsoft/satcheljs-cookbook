14f28df398794ca869c2a677a4d7766ed4d35a9c
Scott Mikula <mikula@gmail.com>
2020-03-24 14:37:39 -0700

    Wrap app in ApolloProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
 import App from './components/App';
+import { client } from './apollo/client';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('app')
+);
